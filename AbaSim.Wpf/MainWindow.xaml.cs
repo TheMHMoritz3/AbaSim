b1a666548278cfcfb72124755ef3f61465f27139
Moritz Herzog <moritz.herzog@german-software-engineers.com>
2017-11-17 19:40:56 +0100

    Added Simulation Window

--- AbaSim.Wpf/MainWindow.xaml.cs
+++ AbaSim.Wpf/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,6 +24,26 @@ namespace AbaSim.Wpf
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void onCompileClicked(object sender, RoutedEventArgs e)
+        {
+            startSimulationWindowThread();
+        }
+
+        private void startSimulationWindowThread()
+        {
+            Thread simulationWindowThread = new Thread(new ThreadStart(openSimulationWindow));
+            simulationWindowThread.SetApartmentState(ApartmentState.STA);
+            simulationWindowThread.IsBackground = true;
+            simulationWindowThread.Start();
+        }
+
+        private void openSimulationWindow()
+        {
+            Window1 simulationWindow = new Window1();
+            simulationWindow.Show();
+            System.Windows.Threading.Dispatcher.Run();
         }
     }
 }
